6aa3f37a5b41d5f44f4c55c8aed8fc882a0593e6
Pascal Bleser <p.bleser@opencloud.eu>
2025-05-02 17:09:25 +0200

    WIP: restructure the Jmap client, and implement the /me/messages Graph API endpoint with it

--- services/graph/pkg/service/v0/service.go
+++ services/graph/pkg/service/v0/service.go
@@ -202,6 +202,8 @@ func NewService(opts ...Option) (Graph, error) { //nolint:maintidx
 		natskv:                   options.NatsKeyValue,
 	}
 
+	gw := NewGroupware(&options.Logger, options.Config)
+
 	if err := setIdentityBackends(options, &svc); err != nil {
 		return svc, err
 	}
@@ -318,6 +320,7 @@ func NewService(opts ...Option) (Graph, error) { //nolint:maintidx
 					r.Patch("/", usersUserProfilePhotoApi.UpsertProfilePhoto(GetUserIDFromCTX))
 					r.Delete("/", usersUserProfilePhotoApi.DeleteProfilePhoto(GetUserIDFromCTX))
 				})
+				r.Get("/messages", gw.GetMessages)
 			})
 			r.Route("/users", func(r chi.Router) {
 				r.Get("/", svc.GetUsers)
